64cc87f83bcd2ae4daa82363a4738655c942a2d5
Dav0105 <120047730+Dav0105@users.noreply.github.com>
2026-08-26 14:28:35 +0200

    Added registration page

--- jobinder/lib/view/register_view.dart
+++ jobinder/lib/view/register_view.dart
@@ -1,38 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:jobinder/view/job_view.dart';
-import 'package:jobinder/view/register_view.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth_provider.dart';
 
-class LoginView extends StatefulWidget {
-  const LoginView({super.key});
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
 
   @override
-  LoginViewState createState() => LoginViewState();
+  RegisterViewState createState() => RegisterViewState();
 }
 
-class LoginViewState extends State<LoginView> {
+class RegisterViewState extends State<RegisterView> {
   // Controllers to manage input fields
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  String? _selectedRole; // For ToggleButtons
+
+  final String _studentRole = "student";
+  final String _employerRole = "employer";
 
   @override
   Widget build(BuildContext context) {
     final authProvider = context.watch<AuthProvider>();
+    final TextTheme textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
+      appBar: AppBar(title: Text('Register')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: Column(
             children: [
+              // Role selection
+              Text('Choose a role', style: textTheme.labelLarge),
+              Wrap(
+                spacing: 10.0,
+                children: [
+                  ChoiceChip(
+                    label: Text('Student'),
+                    selected: _selectedRole == _studentRole,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        _selectedRole = selected ? _studentRole : null;
+                      });
+                    },
+                  ),
+                  ChoiceChip(
+                    label: Text('Employer'),
+                    selected: _selectedRole == "employer",
+                    onSelected: (bool selected) {
+                      setState(() {
+                        _selectedRole = selected ? _employerRole : null;
+                      });
+                    },
+                  ),
+                ],
+              ),
 
+              // Email
               TextFormField(
                 controller: _emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
@@ -48,6 +75,8 @@ class LoginViewState extends State<LoginView> {
                 },
               ),
               const SizedBox(height: 16),
+
+              // Password
               TextFormField(
                 controller: _passwordController,
                 decoration: const InputDecoration(labelText: 'Password'),
@@ -63,35 +92,32 @@ class LoginViewState extends State<LoginView> {
                 },
               ),
               const SizedBox(height: 20),
-              if (authProvider.errorMessage != null) ...[
-                Text(
-                  authProvider.errorMessage!,
-                  style: TextStyle(color: Theme.of(context).colorScheme.error),
-                ),
-                const SizedBox(height: 10),
-              ],
+
+              // Register button
               ElevatedButton(
-                onPressed: authProvider.isLoading ? null : () => _authenticate(context),
+                onPressed: authProvider.isLoading
+                    ? null
+                    : () => _authenticate(context),
                 child: authProvider.isLoading
                     ? const SizedBox(
                         height: 20,
                         width: 20,
                         child: CircularProgressIndicator(strokeWidth: 2),
                       )
-                    : Text('Login'),
+                    : Text("Register"),
               ),
 
+              // Return to login button
               TextButton(
                 onPressed: authProvider.isLoading
                     ? null
                     : () {
                         context.read<AuthProvider>().clearError();
-                        Navigator.push(
+                        Navigator.pop(
                           context,
-                          MaterialPageRoute(builder: (_) => RegisterView()),
-                        );
+                        ); // Navigate back to the previous screen
                       },
-                child: Text("Don't have an account?"),
+                child: Text('Already have an account?'),
               ),
             ],
           ),
@@ -111,12 +137,20 @@ class LoginViewState extends State<LoginView> {
 
     final navigator = Navigator.of(context);
 
-    final success = await authProvider.signInWithEmailAndPassword(email, password);
+    final success = await authProvider.registerWithEmailAndPassword(
+      email,
+      password,
+    );
 
     if (success) {
-      navigator.pushReplacement(
-        MaterialPageRoute(builder: (_) => const JobView()),
+      // Check if widget is still mounted before showing SnackBar
+      if (!context.mounted) return;
+
+      // Display toaster at the bottom of the screen
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Account successfully created!")),
       );
+      navigator.pop(); // Navigate back to the previous screen
     }
   }
 }
