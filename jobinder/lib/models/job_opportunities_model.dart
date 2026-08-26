e695dc86d405f91c215b31c1186fd87b9cd1953c
rdtjulien <jujurdt@gmail.com>
2026-08-26 14:42:40 +0200

    feat/create-job: Create model for job opportunities

---
+++ jobinder/lib/models/job_opportunities_model.dart
@@ -0,0 +1,54 @@
+class JobOpportunities {
+  final String id;
+  final String adress;
+  final String canton;
+  final String city;
+  final String degree;
+  final String description;
+  final List<String> languages;
+  final String name;
+  final int salary;
+  final DateTime timestamp;
+
+  JobOpportunities({
+    required this.id,
+    required this.adress,
+    required this.canton,
+    required this.city,
+    required this.degree,
+    required this.description,
+    required this.languages,
+    required this.name,
+    required this.salary,
+    required this.timestamp,
+  });
+
+  factory JobOpportunities.fromMap(Map<String, dynamic> data, String docId) {
+    return JobOpportunities(
+      id: docId,
+      adress: data['adress'] ?? '',
+      canton: data['canton'] ?? '',
+      city: data['city'] ?? '',
+      degree: data['degree'] ?? '',
+      description: data['description'] ?? '',
+      languages: List<String>.from(data['languages'] ?? []),
+      name: data['name'] ?? '',
+      salary: data['salary'] ?? 0,
+      timestamp: data['timestamp'] ?? DateTime,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'adress': adress,
+      'canton': canton,
+      'city': city,
+      'degree': degree,
+      'description': description,
+      'languages': languages,
+      'name': name,
+      'salary': salary,
+      'timestamp': timestamp,
+    };
+  }
+}
